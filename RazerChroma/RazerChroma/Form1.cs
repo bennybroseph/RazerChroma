25d4f10241c6fa8cd6faa176eb59dca57b0a8abe
Benjamin Odom <bennybroseph@gmail.com>
2016-01-13 23:11:55 -0600

    0.3
    Added helpful comments

--- RazerChroma/RazerChroma/Form1.cs
+++ RazerChroma/RazerChroma/Form1.cs
@@ -8,9 +8,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-using Corale.Colore.Core;
-using ColoreColor = Corale.Colore.Core.Color;
-using Corale.Colore.Razer.Keypad;
+using Corale.Colore.Core;                           // To easily get to the Chroma object
+using ColoreColor = Corale.Colore.Core.Color;       // To easily get to the Color object of the API and also distinguish it from C#'s Color            
+
+/*
+   TODO: Finish functionality of selecting different effects
+         Add Spectrum Cycling
+         Add custom colors
+         Add saving of colors
+*/
 
 namespace RazerChroma
 {
@@ -20,6 +26,8 @@ namespace RazerChroma
         {
             InitializeComponent();
 
+            // Adds an object from the MyColor class which contains a string for the ComboBox and
+            // a ColoreColor object to easily set things later based on the user's selection
             ColorSelector.Items.Add(new MyColor(ColoreColor.Red, "Red"));
             ColorSelector.Items.Add(new MyColor(ColoreColor.Green, "Green"));
             ColorSelector.Items.Add(new MyColor(ColoreColor.Blue, "Blue"));
@@ -27,12 +35,14 @@ namespace RazerChroma
 
             ColorSelector.SelectedIndex = 0;
 
+            // Copy the previous Items into the second ComboBox
             object[] oContainer = new object[ColorSelector.Items.Count];
             ColorSelector.Items.CopyTo(oContainer, 0);
             ColorSelector2.Items.AddRange(oContainer);
 
             ColorSelector2.SelectedIndex = 0;
 
+            // Name the strings for each device and store them into a ComboBox
             InstanceSelector.Items.Add("All");
             InstanceSelector.Items.Add("Mouse");
             InstanceSelector.Items.Add("Keyboard");
@@ -42,6 +52,7 @@ namespace RazerChroma
 
             InstanceSelector.SelectedIndex = 0;
 
+            // Name the strings for each effect and store them into a ComboBox
             EffectSelector.Items.Add("Static");
             EffectSelector.Items.Add("Breathing");
             EffectSelector.Items.Add("Wave");
@@ -49,6 +60,7 @@ namespace RazerChroma
 
             EffectSelector.SelectedIndex = 0;
 
+            // Hide the second color since it can only be used with certain effects
             ColorSelector2.Hide();
         }
 
@@ -56,8 +68,10 @@ namespace RazerChroma
         {
             switch (e.Button)
             {
+                // Only do things when the user clicks with the left button
                 case MouseButtons.Left:
                     {
+                        // Parse which MyColor object the user selected in each box
                         MyColor oSelectedColor = ColorSelector.SelectedItem as MyColor;
                         MyColor oSelectedColor2 = ColorSelector2.SelectedItem as MyColor;
 
@@ -66,27 +80,31 @@ namespace RazerChroma
                             case "All":
                                 switch (EffectSelector.SelectedItem as string)
                                 {
+                                    // Using SetAll instead of choosing the Instance will set all devices to the static color. No effects
                                     case "Static": Chroma.Instance.SetAll(oSelectedColor.Color); break;
+                                    // Fades in to the first color, then out, then back in to the second color, fade out, repeat
                                     case "Breathing":
                                         {
-                                            Chroma.Instance.Mouse.SetBreathing(oSelectedColor.Color, oSelectedColor2.Color);
-                                            Chroma.Instance.Keyboard.SetBreathing(oSelectedColor.Color, oSelectedColor2.Color);
-                                            Chroma.Instance.Keypad.SetBreathing(oSelectedColor.Color, oSelectedColor2.Color);
+                                            Chroma.Instance.Mouse.SetBreathing(oSelectedColor.Color, oSelectedColor2.Color);        // Not supported on the Naga
+                                            Chroma.Instance.Keyboard.SetBreathing(oSelectedColor.Color, oSelectedColor2.Color);     
+                                            Chroma.Instance.Keypad.SetBreathing(oSelectedColor.Color, oSelectedColor2.Color);       // A Keypad is the Razer Orbweaver Chroma or the Razer Tartus Chroma
                                             Chroma.Instance.Mousepad.SetBreathing(oSelectedColor.Color, oSelectedColor2.Color);
-                                            Chroma.Instance.Headset.SetBreathing(oSelectedColor.Color);
+                                            Chroma.Instance.Headset.SetBreathing(oSelectedColor.Color);                             // Only supports one color fading
                                         }
                                         break;
+                                    // Rainbow wave from left to right. Doesn't seem like you can select a color to wave. Also not supported on headsets
                                     case "Wave":
                                         {
-                                            Chroma.Instance.Mouse.SetWave(Corale.Colore.Razer.Mouse.Effects.Direction.BackToFront);
+                                            Chroma.Instance.Mouse.SetWave(Corale.Colore.Razer.Mouse.Effects.Direction.BackToFront);     // Not supported on the Naga
                                             Chroma.Instance.Keyboard.SetWave(Corale.Colore.Razer.Keyboard.Effects.Direction.LeftToRight);
                                             Chroma.Instance.Keypad.SetWave(Corale.Colore.Razer.Keypad.Effects.Direction.LeftToRight);
                                             Chroma.Instance.Mousepad.SetWave(Corale.Colore.Razer.Mousepad.Effects.Direction.LeftToRight);
                                         }
                                         break;
+                                    // Lights up individual keys as you press them. Obviously not supported on Headset and Mousepad
                                     case "Reactive":
                                         {
-                                            Chroma.Instance.Mouse.SetReactive(Corale.Colore.Razer.Mouse.Effects.Duration.Medium, oSelectedColor.Color);
+                                            Chroma.Instance.Mouse.SetReactive(Corale.Colore.Razer.Mouse.Effects.Duration.Medium, oSelectedColor.Color);      // Not supported on the Naga
                                             Chroma.Instance.Keyboard.SetReactive(oSelectedColor.Color, Corale.Colore.Razer.Keyboard.Effects.Duration.Medium);
                                             Chroma.Instance.Keypad.SetReactive(Corale.Colore.Razer.Keypad.Effects.Duration.Medium, oSelectedColor.Color);
                                         }
@@ -95,6 +113,7 @@ namespace RazerChroma
                                     default: break;
                                 }
                                 break;
+                            // SetAll used in this way will set the Instance to the static color. No effects
                             case "Mouse": Chroma.Instance.Mouse.SetAll(oSelectedColor.Color); break;
                             case "Keyboard": Chroma.Instance.Keyboard.SetAll(oSelectedColor.Color); break;
                             case "Keypad": Chroma.Instance.Keypad.SetAll(oSelectedColor.Color); break;
@@ -102,31 +121,31 @@ namespace RazerChroma
                             default: break;
                         }
 
-                        SetColorButton.Enabled = false;
+                        SetColorButton.Enabled = false;     // After applying the setting, make sure the user cannot click the button again
                         break;
                     }
-
-                default: Chroma.Instance.Keypad.SetBreathing(new Corale.Colore.Razer.Keypad.Effects.Breathing(ColoreColor.Blue, ColoreColor.Blue)); break;
+               
+                default: break;     // Otherwise do nothing
             }
         }
 
         private void ColorSelector_SelectedIndexChanged(object sender, EventArgs e)
         {
-            SetColorButton.Enabled = true;
+            SetColorButton.Enabled = true;      // Since a setting changed, make the apply button functional again
         }
 
         private void InstanceSelector_SelectedIndexChanged(object sender, EventArgs e)
         {
-            SetColorButton.Enabled = true;
+            SetColorButton.Enabled = true;      // Since a setting changed, make the apply button functional again
         }
 
         private void EffectSeletor_SelectedIndexChanged(object sender, EventArgs e)
         {
-            SetColorButton.Enabled = true;
+            SetColorButton.Enabled = true;      // Since a setting changed, make the apply button functional again
 
             switch (EffectSelector.SelectedItem as string)
             {
-                case "Breathing": ColorSelector2.Show(); break;
+                case "Breathing": ColorSelector2.Show(); break;     // If the user selected Breathing then they are allowed to choose a second color
                 default: ColorSelector2.Hide(); break;
             }
         }
